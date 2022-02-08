269dea9fc3ea55d088857385405461e0dc7071cd
topisteronyango <t.onyango@alustudent.com>
2022-02-08 21:24:36 +0200

    fix test amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -99,6 +99,7 @@ class TestAmenity(unittest.TestCase):
         self.assertEqual(new_d["created_at"], am.created_at.strftime(t_format))
         self.assertEqual(new_d["updated_at"], am.updated_at.strftime(t_format))
 
+        
     def test_str(self):
         """test that the str method has the correct output"""
         amenity = Amenity()
