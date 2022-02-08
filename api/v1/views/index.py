1cc1412209d130e3e60861b11aacceb46e2f2a59
topisteronyango <t.onyango@alustudent.com>
2022-02-08 10:51:43 -0800

    index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""following directions"""
+"""index module"""
+
+from flask import Flask, Blueprint, jsonify
 from api.v1.views import app_views
-from flask import jsonify, Blueprint, render_template, abort
 from models import storage
 from models.amenity import Amenity
 from models.city import City
@@ -10,18 +11,26 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
+classes = {
+    "amenities": Amenity,
+    "cities": City,
+    "places": Place,
+    "reviews": Review,
+    "states": State,
+    "users": User
+}
 
-@app_views.route('/status', methods=['GET'])
-def status():
-    """status"""
-    return (jsonify({"status": "OK"}))
+
+@app_views.route('/status')
+def json_returned():
+    """returns a JSON: '"status": "OK"' """
+    return jsonify(status="OK")
 
 
-@app_views.route('/stats', methods=['GET'])
+@app_views.route('/stats')
 def stats():
-    return (jsonify({"amenities": storage.count(Amenity),
-                     "cities": storage.count(City),
-                     "places": storage.count(Place),
-                     "reviews": storage.count(Review),
-                     "states": storage.count(State),
-                     "users": storage.count(User)}))
+    """retrieves the number of each objects by type"""
+    objs = {}
+    for key, value in classes.items():
+        objs[key] = storage.count(value)
+    return jsonify(objs)
